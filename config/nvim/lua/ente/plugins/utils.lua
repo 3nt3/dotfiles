3933af2eb4fb02569c1acddbb6d24e8ed7047c27
3nt3 <gott@3nt3.de>
2026-01-26 14:09:38 +0100

    idk
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- config/nvim/lua/ente/plugins/utils.lua
+++ config/nvim/lua/ente/plugins/utils.lua
@@ -5,7 +5,37 @@ return {
         "max397574/better-escape.nvim",
         event = "InsertEnter",
         opts = {
-            mapping = { "jk", "jj" },
+            mappings = {
+                -- i for insert
+                i = {
+                    j = {
+                        -- These can all also be functions
+                        k = "<Esc>",
+                        j = "<Esc>",
+                    },
+                },
+                c = {
+                    j = {
+                        k = "<C-c>",
+                        j = "<C-c>",
+                    },
+                },
+                t = {
+                    j = {
+                        k = "<C-\\><C-n>",
+                    },
+                },
+                v = {
+                    j = {
+                        k = "<Esc>",
+                    },
+                },
+                s = {
+                    j = {
+                        k = "<Esc>",
+                    },
+                },
+            },
             timeout = 200,
         },
     },
