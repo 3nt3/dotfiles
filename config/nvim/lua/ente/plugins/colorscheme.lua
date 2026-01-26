bb6c7a26ce3be2fbcbeffc1adb8191bf04efc6be
3nt3 <gott@3nt3.de>
2026-01-26 14:07:46 +0100

    add gruvbox
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- config/nvim/lua/ente/plugins/colorscheme.lua
+++ config/nvim/lua/ente/plugins/colorscheme.lua
@@ -46,11 +46,39 @@ return {
                     },
                 },
             })
-            -- Set the colorscheme
-            vim.cmd.colorscheme("catppuccin")
         end,
     },
 
     -- Gruvbox
-    { "ellisonleao/gruvbox.nvim", lazy = true },
+    {
+        "ellisonleao/gruvbox.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("gruvbox").setup({
+                terminal_colors = false, -- add neovim terminal colors
+                undercurl = true,
+                underline = true,
+                bold = true,
+                italic = {
+                    strings = true,
+                    emphasis = true,
+                    comments = true,
+                    operators = false,
+                    folds = true,
+                },
+                strikethrough = true,
+                invert_selection = false,
+                invert_signs = false,
+                invert_tabline = false,
+                inverse = true,      -- invert background for search, diffs, statuslines and errors
+                contrast = "medium", -- can be "hard", "soft" or empty string
+                palette_overrides = {},
+                overrides = {},
+                dim_inactive = false,
+                transparent_mode = false,
+            })
+            vim.cmd("colorscheme gruvbox")
+        end
+    },
 }
